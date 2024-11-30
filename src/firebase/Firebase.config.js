7ee5f87a24392a7b60493805c412abe9e4c440a1
mohammaduzzal <aalp72142@gmail.com>
2024-11-30 18:17:43 +0600

    added firebase secure file

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDRmyMjlTc1EOvQweTeptkmZye0QPD-ECw",
-  authDomain: "coffee-store-f4d27.firebaseapp.com",
-  projectId: "coffee-store-f4d27",
-  storageBucket: "coffee-store-f4d27.firebasestorage.app",
-  messagingSenderId: "687515590665",
-  appId: "1:687515590665:web:f18c18b59cfb5ee7d458d8"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
